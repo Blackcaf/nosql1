0f88985c7129a6ba3e50b9ef2ebf6898acc9f2f7
Blackcaf <144961322+Blackcaf@users.noreply.github.com>
2026-09-22 20:25:46 +0300

    Add PostgreSQL schema, Docker and Etcd adapter

---
+++ src/main/java/ru/library/config/EtcdConfig.java
@@ -0,0 +1,13 @@
+package ru.library.config;
+
+import io.etcd.jetcd.Client;
+import io.etcd.jetcd.KV;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EtcdConfig {
+    @Bean(destroyMethod="close")
+    Client etcdClient(AppProperties props){return Client.builder().endpoints(props.etcd().endpoint()).build();}
+    @Bean KV etcdKv(Client client){return client.getKVClient();}
+}
